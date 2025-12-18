5ed6d508992aaddb48c5d6553044be75c227ea70
John Doe <danielnaydenov05@gmail.com>
2025-12-18 17:06:44 +0100

    WP5.2 corrected

--- WP5.2.py
+++ WP5.2.py
@@ -12,3 +12,22 @@ from functions import *
 from package4 import *
 
 
+stressArr = np.empty_like(y_linspace)
+
+y_linspaceShort = np.linspace(0,(b/2)*(3/4),250)
+
+maxStress = 450
+
+yVals = [0,2.14,4.28,6.42,8.56,10.7]
+stressVals = [128.28,111.97,87.16,53.91,17.96,0.42]
+
+Stress = sp.interpolate.CubicSpline(yVals,stressVals)
+for i,y in enumerate(y_linspaceShort):
+    stressArr[i]=Stress(y)
+
+margin = maxStress/stressArr
+
+plt.plot(y_linspaceShort,margin)
+plt.xlabel("Span Location[m]")
+plt.ylabel("Margin of Safety[-]")
+plt.show()
